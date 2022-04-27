b56c34adff74c3fc57273b7b8195f160fb1ac30f
Mahesh K <mkandegedara.c@rex.com>
2022-04-28 01:30:35 +0530

    Updates

---
+++ precache-manifest.82bd10bced50df238e45b9d303885fe5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3639f17545d4b55d0a8137329de0d3b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "27be68a9efdb92e054fd",
+    "url": "/my-app/static/css/2.61833881.chunk.css"
+  },
+  {
+    "revision": "c68ba80a7a18d3b1032e",
+    "url": "/my-app/static/css/main.06307434.chunk.css"
+  },
+  {
+    "revision": "27be68a9efdb92e054fd",
+    "url": "/my-app/static/js/2.432e01cc.chunk.js"
+  },
+  {
+    "revision": "3f2e1ff0d15dc835e00724af8791d834",
+    "url": "/my-app/static/js/2.432e01cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68ba80a7a18d3b1032e",
+    "url": "/my-app/static/js/main.6b9770f0.chunk.js"
+  },
+  {
+    "revision": "7b27bf594e4f003582bb",
+    "url": "/my-app/static/js/runtime-main.5ba1a8c7.js"
+  },
+  {
+    "revision": "52160a9eae5b2a8cc899c147f7045044",
+    "url": "/my-app/static/media/protesters.52160a9e.png"
+  },
+  {
+    "revision": "d64d1cc3307f1a3e99f6babda219bee3",
+    "url": "/my-app/static/media/safe-protests.d64d1cc3.png"
+  }
+]);
