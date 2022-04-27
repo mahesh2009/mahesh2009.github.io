b150dfbeec4225068885d593f2f9f1c453d7a6a5
Mahesh K <mkandegedara.c@rex.com>
2022-04-27 13:06:59 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Routes from "./Routes";
+import MainNav from "./pages/Navbar/MainNav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		loggedIn: false,
+		users: [],
+	};
+
+	logOut = () => {
+		this.setState({ loggedIn: false }, () =>
+			console.log("Logged In:", this.state.loggedIn)
+		);
+	};
+
+	setUser = (user) => {
+		let user_pool = this.state.users.concat(user);
+		this.setState({ users: user_pool }, () =>
+			console.log("UserPool: ", this.state.users)
+		);
+	};
+
+	setLogin = (isLoggedIn) => {
+		this.setState({ loggedIn: isLoggedIn });
+		console.log(this.state.loggedIn);
+	};
+
+	render() {
+		return (
+			<div>
+				<MainNav loggedIn={this.state.loggedIn} logOut={this.logOut} />
+				<Routes
+					loggedIn={this.state.loggedIn}
+					setLogin={this.setLogin}
+					users={this.state.users}
+					setUser={this.setUser}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
