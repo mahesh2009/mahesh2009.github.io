338e11e2869d1fb55b9793922250350825893bb3
Mahesh K <mkandegedara.c@rex.com>
2022-04-28 01:38:49 +0530

    Updates

---
+++ precache-manifest.749a53198f60aea0cbb240eb83e15cfd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065104921641c2d1a7b1fc518f2ba64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27be68a9efdb92e054fd",
+    "url": "/static/css/2.61833881.chunk.css"
+  },
+  {
+    "revision": "f40e594b3850f10f9815",
+    "url": "/static/css/main.3e49e2b1.chunk.css"
+  },
+  {
+    "revision": "27be68a9efdb92e054fd",
+    "url": "/static/js/2.432e01cc.chunk.js"
+  },
+  {
+    "revision": "3f2e1ff0d15dc835e00724af8791d834",
+    "url": "/static/js/2.432e01cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40e594b3850f10f9815",
+    "url": "/static/js/main.0180851a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "52160a9eae5b2a8cc899c147f7045044",
+    "url": "/static/media/protesters.52160a9e.png"
+  },
+  {
+    "revision": "d64d1cc3307f1a3e99f6babda219bee3",
+    "url": "/static/media/safe-protests.d64d1cc3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.82bd10bced50df238e45b9d303885fe5.js"
+  "/precache-manifest.749a53198f60aea0cbb240eb83e15cfd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
